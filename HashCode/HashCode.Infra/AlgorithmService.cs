ac5efc7544e5d3cc10ca25df47daff2edcf70596
Bram <bdt@emakina.com>
2020-02-20 19:59:41 +0100

    algo 2 done

--- HashCode/HashCode.Infra/AlgorithmService.cs
+++ HashCode/HashCode.Infra/AlgorithmService.cs
@@ -1,4 +1,5 @@
 ﻿using HashCode.Core.Domain;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -19,6 +20,8 @@ namespace HashCode.Infra
             List<MaxThroughputLibrary> maxThrouhgputItem = new List<MaxThroughputLibrary>();
             foreach(var library in _input.Libraries)
             {
+
+
                 var daysToScan = _input.Days - library.SignUpProcess;
                 var scannableBooks = daysToScan * library.BooksPerDay;
 
@@ -32,10 +35,28 @@ namespace HashCode.Infra
                 //    LibraryId = library.Id,
                 //    Throughput = scannableBooks
                 //});
+
             }
 
             maxThrouhgputItem.OrderByDescending(x => x.Throughput);
-        }       
+        }
+        public List<CalculatedLibrary> RunAlgorithmNumberOne()
+        {
+            List<CalculatedLibrary> inputLibraries = new List<CalculatedLibrary>();
+            foreach (var library in _input.Libraries)
+            {
+                inputLibraries.Add(new CalculatedLibrary() { AmountOfBooks = library.AmountOfBooks, BookIds = library.BookIds, SignUpProcess = library.SignUpProcess, TotalTimeNeeded = library.SignUpProcess + library.AmountOfBooks / library.BooksPerDay });
+                
+            }
+            var sorted =  inputLibraries.OrderBy(x => x.SignUpProcess).OrderByDescending(x => x.TotalTimeNeeded).ToList();
+            List<Tuple<int, int[]>> tuple = new List<Tuple<int, int[]>>;
+            
+            foreach(var sort in sorted)
+            {
+                tuple.Add(sort.li)
+            }
+        }
 
-	}
+
+    }
 }
