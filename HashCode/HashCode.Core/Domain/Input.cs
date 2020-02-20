44b12df74d1ab66bbdf97d6f31bf5413b57aff7d
Jente Vets <C00148@brusselsairlines.com>
2020-02-20 19:06:35 +0100

    domain obj

--- HashCode/HashCode.Core/Domain/Input.cs
+++ HashCode/HashCode.Core/Domain/Input.cs
@@ -1,6 +1,13 @@
-﻿namespace HashCode.Core.Domain
+﻿using System.Collections.Generic;
+
+namespace HashCode.Core.Domain
 {
 	public class Input
 	{
+		public int NumberOfBooks { get; set; }
+		public int NumberOfLibraries { get; set; }
+		public int Days { get; set; }
+		public List<int> Books { get; set; }
+		public List<Library> Libraries { get; set; }
 	}
 }
---
+++ HashCode/HashCode.Core/Domain/Library.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace HashCode.Core.Domain
+{
+	public class Library
+	{
+		public int AmountOfBooks { get; set; }
+		public int SignUpProcess { get; set; }
+		public int BooksPerDay { get; set; }
+		public List<int> BookIds { get; set; }
+	}
+}
